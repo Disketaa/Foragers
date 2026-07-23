dc5024d997f5a035cab7f0fc4be73d92697d509e
Disketaa <76956762+Disketaa@users.noreply.github.com>
2026-07-23 16:36:31 +0300

    Add carrot vegetable object

---
+++ Foragers/Content/Assets/Sprites/Props/Carrot.lua
@@ -0,0 +1,11 @@
+return {
+	extends = "Content.Assets.Sprites.Props._Vegetables",
+	components = {
+		{
+			component = "drop",
+			drops = {
+				{ sprite = "Content/Assets/Sprites/Drops/Carrot" },
+			},
+		},
+	},
+}
